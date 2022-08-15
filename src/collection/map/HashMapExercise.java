6f450e3fe457675406659a4d73764e609b8cbd79
beakHyeonMyeong <81284265+whitewise95@users.noreply.github.com>
2022-08-16 00:14:42 +0900

    hashMap ->  putAll 과 containsKey내용도 추가

--- src/collection/map/HashMapExercise.java
+++ src/collection/map/HashMapExercise.java
@@ -9,15 +9,19 @@ public class HashMapExercise {
 
          map.put("짱구", 200);   // map.put(키,값) 으로 저장하게 된다.
          map.put("철수", 400);
-         map.put("유리", 300);
-         map.put("맹구", 100);
-         map.put("훈이", 100);
 
          System.out.println(map);  //map의 키의 데이터타입은 String, Integer은 기본적으로 toString이 구현되어 있어 값이 출력된다.
          System.out.println(map.get("짱구"));      // get 메소드로 키를 입력하면 값이 나온다.
          System.out.println(map.get("철수"));
-         System.out.println(map.get("유리"));
-         System.out.println(map.get("맹구"));
-         System.out.println(map.get("훈이"));
+
+         System.out.println(map.containsKey("철수"));  // containsKey 메소드는 키값에 철수가 있는지 확인하는 메소드다
+
+         //========================================================
+
+         Map<String, Integer> newMap = new HashMap();
+
+         newMap.putAll(map);  // putAll() 은 같은 데이터타입을 가진 다른 Map을 전부 포함하게 된다.
+
+         System.out.println(newMap);
     }
 }
