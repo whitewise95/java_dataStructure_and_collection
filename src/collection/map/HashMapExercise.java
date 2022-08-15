2a204d6586934db3c8d75baeccc31d1ec040c347
beakHyeonMyeong <81284265+whitewise95@users.noreply.github.com>
2022-08-16 00:30:18 +0900

    설명하는 방식을 수정

--- src/collection/map/HashMapExercise.java
+++ src/collection/map/HashMapExercise.java
@@ -5,23 +5,52 @@ import java.util.*;
 public class HashMapExercise {
 
      public static void main(String[] args) {
-         Map<String, Integer> map = new HashMap();  //Map<"key의 데이터타입", value의 데이터입> 변수명 으로 선언
+         /*
+         * 1) Map<"key의 데이터타입", value의 데이터입> 변수명 = new HashMap(); 으로 생성
+         * 2) put()  =  put(키,값) 으로 MapCollection에 저장된다.
+         * 3) get()  =   get(키) 메소드로 키를 입력하면 값이 나온다.
+         * 4) containsKey()   =  containsKey(키) 메소드는 해당 키에 철수가 있는지 확인하는 메소드다
+         * */
 
-         map.put("짱구", 200);   // map.put(키,값) 으로 저장하게 된다.
+         //1
+         Map<String, Integer> map = new HashMap();
+
+         //2
+         map.put("짱구", 200);
          map.put("철수", 400);
 
-         System.out.println(map);  //map의 키의 데이터타입은 String, Integer은 기본적으로 toString이 구현되어 있어 값이 출력된다.
-         System.out.println(map.get("짱구"));      // get 메소드로 키를 입력하면 값이 나온다.
+         //3  {철수=400, 짱구=200},  200,  400
+         System.out.println(map);
+         System.out.println(map.get("짱구"));
          System.out.println(map.get("철수"));
 
-         System.out.println(map.containsKey("철수"));  // containsKey 메소드는 키값에 철수가 있는지 확인하는 메소드다
+         //4  true
+         System.out.println(map.containsKey("철수"));
 
-         //========================================================
 
+        /*
+        * 1) putAll()   = putAll(new HashMap())  ->  putAll() 은 같은 데이터타입을 가진 다른 Map을 전부 포함하게 된다.
+        * 2) Foreach()  = Map은 Foreach() 메소드를 이용하여 순환하면서 키-값 을 꺼낼 수 있다.
+        * 3) keySet()   = Map은 index가 없지만 ketSet()를 이용하면 key를 set으로 넘겨주어 Map에 존재하는 키를 모두 순회할 수 있다.
+        * */
          Map<String, Integer> newMap = new HashMap();
 
-         newMap.putAll(map);  // putAll() 은 같은 데이터타입을 가진 다른 Map을 전부 포함하게 된다.
-
+         //1 {철수=400, 짱구=200}
+         newMap.putAll(map);
          System.out.println(newMap);
+
+         //2  key = 철수 value = 400,   key = 짱구 value = 200
+         newMap.forEach((key,value) -> System.out.println("key = " + key + " value = " + value));
+
+
+         //3  철수  짱구
+         for (String key : newMap.keySet()){
+             System.out.println(key);
+         }
+
+
+
+
+
     }
 }
