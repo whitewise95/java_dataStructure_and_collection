52c24c75cac6185085bb0b09cc82b51d9ff5bd98
beakHyeonMyeong <81284265+whitewise95@users.noreply.github.com>
2022-08-16 23:29:44 +0900

    상수에 final과 변수명 대문자로 변경

--- src/singletonPatton/SafeSingleton_constructor.java
+++ src/singletonPatton/SafeSingleton_constructor.java
@@ -1,7 +1,7 @@
 package singletonPatton;
 
 public class SafeSingleton_constructor {
-    private static SafeSingleton_constructor safeSingleton_constructor = new SafeSingleton_constructor();
+    private static final SafeSingleton_constructor SAFE_SINGLETON_CONSTRUCTOR = new SafeSingleton_constructor();
 
     private SafeSingleton_constructor() {
     }
@@ -10,6 +10,6 @@ public class SafeSingleton_constructor {
      * 컴파일 때 부터 생성한 인스터스를 넘겨주는 방법이다.
      * */
     public static SafeSingleton_constructor getInstance() {
-        return safeSingleton_constructor;
+        return SAFE_SINGLETON_CONSTRUCTOR;
     }
 }
