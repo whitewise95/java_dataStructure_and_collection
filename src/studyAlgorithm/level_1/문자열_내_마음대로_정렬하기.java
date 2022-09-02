78ad1c804f798a04e7608fa306e1d5ae76a59590
beakHyeonMyeong <81284265+whitewise95@users.noreply.github.com>
2022-09-02 23:35:03 +0900

    문자열 내 마음대로 정렬하기 정답풀이 하나씩 해보기 및 체육복 여러 시도

--- src/studyAlgorithm/level_1/문자열_내_마음대로_정렬하기.java
+++ src/studyAlgorithm/level_1/문자열_내_마음대로_정렬하기.java
@@ -1,7 +1,6 @@
 package studyAlgorithm.level_1;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /*
  * https://school.programmers.co.kr/learn/courses/30/lessons/12915
@@ -10,7 +9,7 @@ import java.util.stream.Collectors;
 public class 문자열_내_마음대로_정렬하기 {
 
     public static void main(String[] args) {
-        printTest(solution(new String[] { "abcaef", "abcdea", "cdx", "abcaee", "abcced", "abccec" }, 2));
+        printTest(test2(new String[] { "abcaef", "abcdea", "cdx", "abcaee", "abcced", "abccec" }, 2));
         //        printTest(test(new String[] { "sun", "bed", "car" }, 1));
     }
 
@@ -21,90 +20,63 @@ public class 문자열_내_마음대로_정렬하기 {
     }
 
     public static String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, new Comparator<String>(){
+        Arrays.sort(strings, new Comparator<String>() {
             @Override
-            public int compare(String s1, String s2){
-                if(s1.split("")[n].compareTo(s2.split("")[n]) > 0) return 1;
-                else if(s1.split("")[n].compareTo(s2.split("")[n]) == 0) return s1.compareTo(s2);
-                else if(s1.split("")[n].compareTo(s2.split("")[n]) < 0) return -1;
-                else return 0;
+            public int compare(String s1, String s2) {
+                if (s1.split("")[n].compareTo(s2.split("")[n]) > 0)
+                    return 1;
+                else if (s1.split("")[n].compareTo(s2.split("")[n]) == 0)
+                    return s1.compareTo(s2);
+                else if (s1.split("")[n].compareTo(s2.split("")[n]) < 0)
+                    return -1;
+                else
+                    return 0;
             }
         });
         return strings;
     }
 
-    private static String[] test(String[] strings, int s) {
+    private static String[] test2(String[] strings, int n) {
+        Arrays.sort(strings, (o1, o2) -> {
+            if (o1.split("")[n].compareTo(o2.split("")[n]) > 0)
+                return 1;
+            else if (o1.split("")[n].compareTo(o2.split("")[n]) == 0)
+                return o1.compareTo(o2);
+            else if (o1.split("")[n].compareTo(o2.split("")[n]) < 0)
+                return -1;
+            else
+                return 0;
+        });
+        return strings;
+    }
+
+
+    private static String[] test(String[] strings, int n) {
         for (int i = 0; i < strings.length - 1; i++) {
-            int cnt = s;  //몇번 반복하는지 확인하기 위함
-            int minIndex = i;
-            for (int j = i; j < strings.length - 1; j++) {
-                int value = strings[minIndex].split("")[s].compareTo(strings[j + 1].split("")[s]);
-                if (value > 0) {
-                    minIndex = j + 1;
-                } else if (value == 0) {
-                    while (value == 0) {
-                        try {
-                            value = strings[minIndex].split("")[++cnt].compareTo(strings[j + 1].split("")[cnt]);
-                            if (value > 0) {
-                                minIndex = j + 1;
-                            }
-                        } catch (Exception e) {
-                            cnt = s;
-                            while (value == 0) {
-                                try {
-                                    value = strings[minIndex].split("")[--cnt].compareTo(strings[j + 1].split("")[cnt]);
-                                    if (value > 0) {
-                                        minIndex = j + 1;
-                                    }
-                                } catch (Exception d) {
-                                    value = 1;//                            value = 1;
-                                }
-                            }
-                        }
+            for (int j = i + 1; j < strings.length; j++) {
+                if (strings[i].charAt(n) > strings[j].charAt(n)) {
+                    sort(strings, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < strings.length - 1; i++) {
+            for (int j = i + 1; j < strings.length; j++) {
+                if (strings[i].charAt(n) == strings[j].charAt(n)) {
+                    if (strings[i].compareTo(strings[j]) > 0) {
+                        sort(strings, i, j);
                     }
                 }
             }
-
-            String temp = strings[i];
-            strings[i] = strings[minIndex];
-            strings[minIndex] = temp;
-
         }
         return strings;
     }
 
-    public static String[] solution2(String[] strings, int n) {
-        String[] answer = strings;
+    private static void sort(String[] strings, int i, int j) {
+        StringBuffer stringBuffer = new StringBuffer();
 
-        for(int i = 0;i<answer.length-1;i++){
-            String temp = "";
-          /*
-          for(int j= 0;j<answer.length;j++){
-              System.out.println(answer[j]);
-          }
-          */
-            for(int k= i+1;k<answer.length;k++){
-                if(answer[i].charAt(n) > answer[k].charAt(n)){
-                    temp = answer[i];
-                    answer[i] = answer[k];
-                    answer[k] = temp;
-                }
-            }
-        }
-        for(int i = 0;i<answer.length-1;i++){
-            String temp = "";
-            for(int k= i+1;k<answer.length;k++){
-                if(answer[i].charAt(n) == answer[k].charAt(n)){
-                    if(answer[i].compareTo(answer[k])>0){
-                        temp = answer[i];
-                        answer[i] = answer[k];
-                        answer[k] = temp;
-                    }
-                }
-            }
-        }
-
-        return answer;
+        stringBuffer.append(strings[i]);
+        strings[i] = strings[j];
+        strings[j] = stringBuffer.toString();
     }
-
 }
--- src/studyAlgorithm/level_1/체육복.java
+++ src/studyAlgorithm/level_1/체육복.java
@@ -1,7 +1,7 @@
 package studyAlgorithm.level_1;
 
 import java.util.*;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 /*
  * https://school.programmers.co.kr/learn/courses/30/lessons/42862
@@ -9,33 +9,66 @@ import java.util.stream.IntStream;
 public class 체육복 {
 
     public static void main(String[] args) {
-        System.out.println(solution(10, new int[] { 1,2,3 }, new int[] { 1,2,7 }));
+        System.out.println(solution(5, new int[] { 2, 4 }, new int[] { 2, 4, 5 }));
+        System.out.println(solution(5, new int[] { 2, 4 }, new int[] { 1, 3, 5 }));
+        System.out.println(solution(5, new int[] { 2, 4 }, new int[] { 3 }));
+        System.out.println(solution(3, new int[] { 3 }, new int[] { 1 }));
     }
 
-    static public int solution(int n, int[] lost, int[] reserve) {
-        int cnt= 0,again = 0;
-        List<Integer> tempList = new ArrayList<>();
-        for (int value : reserve){
-            tempList.add(value);
+    static public int solution(int n, int[] lostArray, int[] reserveArray) {
+        List<Integer> i1 = Arrays.stream(lostArray).boxed().collect(Collectors.toList());
+
+        List<Integer> test2 = i1.stream().collect(Collectors.toList());
+
+        List<Integer> test3 = i1.stream().filter(integer ->
+                !Arrays.stream(reserveArray).boxed().collect(Collectors.toList()).stream().anyMatch(lost -> {
+                            if (integer == lost) {
+                                test2.remove(integer);
+                                return true;
+                            }
+                            return false;
+                        }
+                )
+        ).collect(Collectors.toList());
+
+        if (test2.size() == 0) {
+            return n;
+        } else if (test3.size() == 0 && test2.size() == 0) {
+            return n;
+        } else if (test3.size() == 0 && test2.size() != 0) {
+            return n - test2.size();
+        } else if (test3.size() != 0 && test2.size() == 0) {
+            return n;
         }
 
-        Iterator iterator = tempList.listIterator();
-        boolean isAgain = false;
-        for (Integer rValue : lost) {
-            if(iterator.hasNext()){
-
-                int value =  isAgain ? again : (int) iterator.next();
-
-                if(value == rValue || value - 1 == rValue || value + 1 == rValue) {
+        Iterator<Integer> integers = Arrays.stream(reserveArray).boxed().collect(Collectors.toList()).iterator();
+        int cnt = 0;
+        for (int lost : lostArray) {
+            int before = -1;
+            if(before == -1){
+                if (integers.hasNext()) {
+                    int reserve = integers.next();
+                    if (reserve == lost - 1 || reserve == lost + 1 || reserve == lost) {
+                        integers.remove();
+                        cnt++;
+                        before = -1;
+                        continue;
+                    } else {
+                        before = reserve;
+                    }
+                }
+            } else {
+                if (before == lost - 1 || before == lost + 1 || before == lost) {
+                    integers.remove();
                     cnt++;
-                    iterator.remove();
-                    isAgain = false;
-                } else {
-                    isAgain = true;
-                    again = value;
+                    before = -1;
+                    continue;
                 }
             }
+
+
         }
-        return n - lost.length + cnt;
+
+        return n - (lostArray.length - cnt);
     }
 }
