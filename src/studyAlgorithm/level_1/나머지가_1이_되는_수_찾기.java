748e3885bcf44de2a58afb4638846735e9558f8c
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2022-09-14 14:41:53 +0900

    프로그래머스 나머지가 1이 되는 수 찾기 문제풀이

--- src/studyAlgorithm/level_1/나머지가_1이_되는_수_찾기.java
+++ src/studyAlgorithm/level_1/나머지가_1이_되는_수_찾기.java
@@ -6,21 +6,21 @@ package studyAlgorithm.level_1;
 public class 나머지가_1이_되는_수_찾기 {
 
     public static void main(String[] args) {
-//        System.out.println(solution(3));
+        System.out.println(solution(3));
         System.out.println(solution2(12));
     }
 
-//    public static Integer solution(Integer n) {
-//        int cnt = 1;
-//        return findMinNum(n, cnt);
-//    }
-//
-//    public static Integer findMinNum(Integer n, Integer cnt) {
-//        if (n % cnt == 1) {
-//            return cnt;
-//        }
-//        return findMinNum(n, ++cnt);
-//    }
+    public static Integer solution(Integer n) {
+        int cnt = 1;
+        return findMinNum(n, cnt);
+    }
+
+    public static Integer findMinNum(Integer n, Integer cnt) {
+        if (n % cnt == 1) {
+            return cnt;
+        }
+        return findMinNum(n, ++cnt);
+    }
 
     public static Integer solution2(Integer n) {
         int cnt = 1;
