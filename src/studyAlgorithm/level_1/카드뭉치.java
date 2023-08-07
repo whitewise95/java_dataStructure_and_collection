dff1bb42044b740c67cb32f5deb91017d827a2de
백현명 <81284265+whitewise95@users.noreply.github.com>
2023-08-07 13:54:52 +0900

    카드뭉치 진행중

--- src/studyAlgorithm/level_1/카드뭉치.java
+++ src/studyAlgorithm/level_1/카드뭉치.java
@@ -14,24 +14,58 @@ public class 카드뭉치 {
 	}
 
 	public static String solution(String[] cards1s, String[] cards2s, String[] goals) {
-		String answer = "";
 		List<String> answers = new ArrayList<>();
-
-		int index = 0;
-		boolean isCards1s = true;
+		int index1 = 0;
+		int index2 = 0;
+		int index3 = 0;
+		Boolean isCards1 = true;
 
 		int maxIndex = Math.max(cards1s.length, cards2s.length);
 
-		for (String goal : goals) {
-			if (isCards1s && cards1s.length -1 > index && cards1s[index++].equals(goal)) {
+		for (int i = 0; i < goals.length; ) {
+
+			if (isCards1) {
+				int addIndex = setup(cards1s, goals[i], answers, index1);
+				if (addIndex - index1 > 0) {
+					index1 = addIndex;
+					i++;
+				} else {
+					isCards1 = false;
+				}
+
+			} else {
+				int addIndex = setup(cards2s, goals[i], answers, index2);
+				if (addIndex - index2 > 0) {
+					index2 = addIndex;
+					i++;
+				} else {
+					isCards1 = true;
+				}
+
 			}
 
+			if (maxIndex <= index1 && maxIndex <= index2 || index3 > cards1s.length + cards2s.length) {
+				break;
+			}
+			index3++;
+		}
 
-			if (maxIndex == index) {
+		return answers.size() == goals.length ? "YES" : "NO";
+	}
+
+	public static Integer setup(String[] cards, String goal, List<String> answers, int index) {
+		if (index > cards.length) {
+			return index;
+		}
+
+		for (int i = index; i < cards.length; i++) {
+			if (goal.equals(cards[index])) {
+				answers.add(cards[index]);
+				index++;
+			} else {
 				break;
 			}
 		}
-
-		return answer;
+		return index;
 	}
 }
