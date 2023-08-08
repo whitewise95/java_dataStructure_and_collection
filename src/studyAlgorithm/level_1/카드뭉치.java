22c9eb1b93fe348e2af4b3ea3c683001a4036a68
백현명 <81284265+whitewise95@users.noreply.github.com>
2023-08-08 13:43:03 +0900

    카드 뭉치 실패

--- src/studyAlgorithm/level_1/카드뭉치.java
+++ src/studyAlgorithm/level_1/카드뭉치.java
@@ -1,71 +1,31 @@
 package studyAlgorithm.level_1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/159994
+ * 실패
+ * 느낌점 - 모든 파라미터에게 반복문 사용하는 것보다 if - else if -  else 문과 index를 이용해서 자연스럽게 반복할 수 있다는 것을 느낄 수 있었다.
  */
 public class 카드뭉치 {
 
 	public static void main(String[] args) {
 		System.out.println(solution(new String[]{"i", "drink", "water"}, new String[]{"want", "to"}, new String[]{"i", "want", "to", "drink", "water"}));
 		System.out.println(solution(new String[]{"i", "water", "drink"}, new String[]{"want", "to"}, new String[]{"i", "want", "to", "drink", "water"}));
+		System.out.println(solution(new String[]{"you", "smarter"}, new String[]{"think", "more", "will", "be"}, new String[]{"think", "more", "you", "will", "be", "smarter"}));
 	}
 
 	public static String solution(String[] cards1s, String[] cards2s, String[] goals) {
-		List<String> answers = new ArrayList<>();
 		int index1 = 0;
 		int index2 = 0;
-		int index3 = 0;
-		Boolean isCards1 = true;
 
-		int maxIndex = Math.max(cards1s.length, cards2s.length);
-
-		for (int i = 0; i < goals.length; ) {
-
-			if (isCards1) {
-				int addIndex = setup(cards1s, goals[i], answers, index1);
-				if (addIndex - index1 > 0) {
-					index1 = addIndex;
-					i++;
-				} else {
-					isCards1 = false;
-				}
-
+		for (String goal : goals) {
+			if (index1 < cards1s.length && goal.equals(cards1s[index1])) {
+				index1++;
+			} else if (index2 < cards2s.length && goal.equals(cards2s[index2])) {
+				index2++;
 			} else {
-				int addIndex = setup(cards2s, goals[i], answers, index2);
-				if (addIndex - index2 > 0) {
-					index2 = addIndex;
-					i++;
-				} else {
-					isCards1 = true;
-				}
-
-			}
-
-			if (maxIndex <= index1 && maxIndex <= index2 || index3 > cards1s.length + cards2s.length) {
-				break;
-			}
-			index3++;
-		}
-
-		return answers.size() == goals.length ? "YES" : "NO";
-	}
-
-	public static Integer setup(String[] cards, String goal, List<String> answers, int index) {
-		if (index > cards.length) {
-			return index;
-		}
-
-		for (int i = index; i < cards.length; i++) {
-			if (goal.equals(cards[index])) {
-				answers.add(cards[index]);
-				index++;
-			} else {
-				break;
+				return "No";
 			}
 		}
-		return index;
+		return "Yes";
 	}
 }
